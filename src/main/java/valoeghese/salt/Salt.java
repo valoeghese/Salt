9286403247ae54f55e23244d837e89b08f16618f
Valoeghese <valoeghesemods@gmail.com>
2023-03-28 20:10:56 +1300

    Start salt (spice at home)

---
+++ src/main/java/valoeghese/salt/Salt.java
@@ -0,0 +1,12 @@
+package valoeghese.salt;
+
+import javax.swing.*;
+
+public class Salt extends JFrame {
+	public static void main(String[] args) {
+		Salt salt = new Salt();
+
+		salt.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		salt.setVisible(true);
+	}
+}
